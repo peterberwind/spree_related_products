5d1a22c2d237dc8fef013d2fd5064a593a0870af
Peterberwind <peterberwind@gmail.com>
2016-09-20 20:43:41 -0400

    Update add_related_product_admin_tabs.rb

--- app/overrides/add_related_product_admin_tabs.rb
+++ app/overrides/add_related_product_admin_tabs.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'add_related_products_admin_tab',
-  insert_bottom: '[data-hook="admin_product_tabs"]',
+  insert_after: '[data-hook="admin_product_tabs"] li',
   partial: 'spree/admin/products/related_products'
 )
